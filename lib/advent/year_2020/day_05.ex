47bd5a61da3a94fcc64b5577ff4157afa803b230
Well Ronfim <wronfim@gmail.com>
2020-12-05 06:24:44 -0500

    mix format

--- lib/advent/year_2020/day_05.ex
+++ lib/advent/year_2020/day_05.ex
@@ -20,20 +20,20 @@ defmodule Advent.Year2020.Day05 do
     |> find_my_seat()
   end
 
-  defp find_row(["B" | tail], {low, high} = range) when low < high - 1, do:
-    find_row(tail, {low + middle(range), high})
+  defp find_row(["B" | tail], {low, high} = range) when low < high - 1,
+    do: find_row(tail, {low + middle(range), high})
 
-  defp find_row(["F" | tail], {low, high} = range) when low < high - 1, do:
-    find_row(tail, {low, high - middle(range)})
+  defp find_row(["F" | tail], {low, high} = range) when low < high - 1,
+    do: find_row(tail, {low, high - middle(range)})
 
   defp find_row(["B" | tail], {_low, high}), do: {Kernel.trunc(high), tail}
   defp find_row(["F" | tail], {low, _high}), do: {Kernel.trunc(low), tail}
 
-  defp find_column(["R" | tail], {low, high}=range) when low < high - 1, do:
-    find_column(tail, {low + middle(range), high})
+  defp find_column(["R" | tail], {low, high} = range) when low < high - 1,
+    do: find_column(tail, {low + middle(range), high})
 
-  defp find_column(["L" | tail], {low, high} = range) when low < high - 1, do:
-    find_column(tail, {low, high - middle(range)})
+  defp find_column(["L" | tail], {low, high} = range) when low < high - 1,
+    do: find_column(tail, {low, high - middle(range)})
 
   defp find_column(["R" | tail], {_low, high}), do: Kernel.trunc(high)
   defp find_column(["L" | tail], {low, _high}), do: Kernel.trunc(low)
